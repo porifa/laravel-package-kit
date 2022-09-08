fcf2634e277ca8916da5b315ae7b03fd6a717cc1
Aamir Sohail KmAs <AamirSohailKmAs@gmail.com>
2022-09-08 21:32:14 +0500

    wip (create testing package migrations)

---
+++ tests/TestingPackage/database/migrations/create_laravel_testing_package_table.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('regular-laravel-testing-package_table', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->timestamps();
+        });
+    }
+};
