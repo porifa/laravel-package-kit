87ed658e66be54938acb3f8b50e5ed089a8b56b5
Aamir Sohail KmAs <AamirSohailKmAs@gmail.com>
2022-09-10 21:42:39 +0500

    wip
    For passing the test

--- tests/Pest.php
+++ tests/Pest.php
@@ -19,17 +19,23 @@ uses()->afterEach(function () {
     if (file_exists($configPath)) {
         unlink($configPath);
     }
+
     collect(File::allFiles(database_path('migrations')))
         ->each(function (SplFileInfo $file) {
             unlink($file->getPathname());
         });
 
-    // collect(File::allFiles(resource_path('views')))
-    //     ->each(function (SplFileInfo $file) {
-    //         unlink($file->getPathname());
-    //     });
-    // collect(File::allFiles(app_path('View/Components')))
-    //     ->each(function (SplFileInfo $file) {
-    //         unlink($file->getPathname());
-    //     });
+    if (file_exists(resource_path('views/vendor'))) {
+        collect(File::allFiles(resource_path('views/vendor')))
+        ->each(function (SplFileInfo $file) {
+            unlink($file->getPathname());
+        });
+    }
+
+    if (file_exists(app_path('View/Components'))) {
+        collect(File::allFiles(app_path('View/Components')))
+        ->each(function (SplFileInfo $file) {
+            unlink($file->getPathname());
+        });
+    }
 })->in(__DIR__);
